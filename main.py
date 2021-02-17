55671bfbabde4c6aa2a2f0e99e4bcce5aa3795f2
migno_chrono <Carlos.BarrosoPenaranda@my.strayer.edu>
2021-02-17 03:07:25 +0000

    i used the same if statement many times...but how do i clean this code so it dosent print the same stuff over and over again?

--- main.py
+++ main.py
@@ -10,6 +10,27 @@ catlocation = int(catlocation)
 print(catlocation)
 if (catlocation == 1):
  print('the cat is on the tree')
+else:
+  print('looking')
+
+
+if (catlocation == 2):
+ print('the cat is on the house')
+else:
+  print('looking')
+
+
+if (catlocation == 3):
+ print('the cat is on the backjard')
+else:
+  print('looking')
+
+
+  #if (catlocation == 2):
+  #print("the cat is on the house")
+  #if (catlocation == 3):
+  #print("the cat is on the backyard")
+   
 
 
 #https://www.youtube.com/watch?v=shO5VbD2rNI
