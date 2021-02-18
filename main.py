4bbdf6698cb6cc7eb4f13d254ab8ca3c3bcd92b9
mignochrono <Carlos.BarrosoPenaranda@my.strayer.edu>
2021-02-18 01:04:03 +0000

    added the while loop for the hole program adition by @dev_with_me

--- main.py
+++ main.py
@@ -1,76 +1,39 @@
-"""
+var = True
+while var == True:
+  print('starting')
+  #ask the question
+  question = "where is the cat?"
+  print(question)
+  #shows options in a list
+  options = [
+      "1.it is on the tree", "2.it is on his house", '3.it is on the backyard',
+      '4.exit'
+  ]
+  #i copied this code from (https://www.geeksforgeeks.org/print-lists-in-python-4-different-ways/) i still dont know why i need the (*) and what does the (sep) means. i can deduce that the ("\n") is a string for new line? i need to do more research
+  print(*options, sep="\n")
+  #i get imput from user
+  catlocation = input()
+  #i change the imput to a integrer number
+  catlocation = int(catlocation)
+  #i use the (if) statement to check if the number stored on the variable "catlocation" is equal to int(1) if not i print "looking"
+  while catlocation < 4:
+    if (catlocation == 1):
+      print('the cat is on the tree')
+      break
 
-print ('starting')
-#ask the question
-question="where is the cat?"
-print(question)
-#shows options in a list
-options=["1.it is on the tree","2.it is on his house",'3.it is on the backyard']
-#i copied this code from (https://www.geeksforgeeks.org/print-lists-in-python-4-different-ways/) i still dont know why i need the (*) and what does the (sep) means. i can deduce that the ("\n") is a string for new line? i need to do more research
-print(*options, sep = "\n")
-#i get imput from user
-catlocation = input()
-#i change the imput to a integrer number
-catlocation = int(catlocation)
-#i use the (if) statement to check if the number stored on the variable "catlocation" is equal to int(1) if not i print "looking"
-if (catlocation == 1):
- print('the cat is on the tree')
-else:
-  print('looking')
+  #i bassically do the same over and over again.  is there a more efficient way to do this?
+    if (catlocation == 2):
+      print('the cat is on the house')
+      break
 
-#i bassically do the same over and over again.  is there a more efficient way to do this? 
-if (catlocation == 2):
- print('the cat is on the house')
-else:
-  print('looking')
+    if (catlocation == 3):
+      print('the cat is on the backyard')
+      break
 
+var = input <= 4
 
-if (catlocation == 3):
- print('the cat is on the backyard')
-else:
-  print('looking')
-
-#extra code not used
-  #elif (catlocation == 2):
-  #print("the cat is on the house")
-  #elif (catlocation == 3):
-  #print("the cat is on the backyard")
-   
-
-
-#https://www.youtube.com/watch?v=shO5VbD2rNI
-"""
-
-print('starting')
-#ask the question
-question = "where is the cat?"
-print(question)
-#shows options in a list
-options = [
-    "1.it is on the tree", "2.it is on his house", '3.it is on the backyard',
-    '4.exit'
-]
-#i copied this code from (https://www.geeksforgeeks.org/print-lists-in-python-4-different-ways/) i still dont know why i need the (*) and what does the (sep) means. i can deduce that the ("\n") is a string for new line? i need to do more research
-print(*options, sep="\n")
-#i get imput from user
-catlocation = input()
-#i change the imput to a integrer number
-catlocation = int(catlocation)
-#i use the (if) statement to check if the number stored on the variable "catlocation" is equal to int(1) if not i print "looking"
-while catlocation < 4:
-	if (catlocation == 1):
-		print('the cat is on the tree')
-		break
-
-#i bassically do the same over and over again.  is there a more efficient way to do this?
-	if (catlocation == 2):
-		print('the cat is on the house')
-		break
-
-	if (catlocation == 3):
-		print('the cat is on the backyard')
-		break
-
+if var != True:
+  print('exiting program')
 #extra code not used
 #elif (catlocation == 2):
 #print("the cat is on the house")
@@ -78,3 +41,5 @@ while catlocation < 4:
 #print("the cat is on the backyard")
 
 #https://www.youtube.com/watch?v=shO5VbD2rNI
+
+
