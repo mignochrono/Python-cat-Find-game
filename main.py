bf7c5258181cb15fe2da8c8f533bc83f19e455e1
migno_chrono <Carlos.BarrosoPenaranda@my.strayer.edu>
2021-02-23 02:10:59 +0000

    FINALLY THE LOOP WORKS!!! I just had to initialize the variable "catlocation"  at the beginning and use the "!=" non equal operator

--- main.py
+++ main.py
@@ -1,39 +1,30 @@
-var = True
-while var == True:
-  print('starting')
-  #ask the question
-  question = "where is the cat?"
-  print(question)
-  #shows options in a list
-  options = [
-      "1.it is on the tree", "2.it is on his house", '3.it is on the backyard',
-      '4.exit'
-  ]
-  #i copied this code from (https://www.geeksforgeeks.org/print-lists-in-python-4-different-ways/) i still dont know why i need the (*) and what does the (sep) means. i can deduce that the ("\n") is a string for new line? i need to do more research
-  print(*options, sep="\n")
-  #i get imput from user
-  catlocation = input()
-  #i change the imput to a integrer number
-  catlocation = int(catlocation)
-  #i use the (if) statement to check if the number stored on the variable "catlocation" is equal to int(1) if not i print "looking"
-  while catlocation < 4:
+catlocation = 0
+while catlocation != 4:
+    print('starting')
+    #ask the question
+    question = "where is the cat?"
+    print(question)
+    #shows options in a list
+    options = ["1.it is on the tree", "2.it is on his house", '3.it is on the backyard','4.exit']
+      #i copied this code from (https://www.geeksforgeeks.org/print-lists-in-python-4-different-ways/) i still dont know why i need the (*) and what does the (sep) means. i can deduce that the ("\n") is a string for new line? i need to do more research
+    print(*options, sep="\n")
+      #i get imput from user
+    catlocation = input()
+      #i change the imput to a integrer number
+    catlocation = int(catlocation)
+      #i use the (if) statement to check if the number stored on the variable "catlocation" is equal to int(1) if not i print "looking"
     if (catlocation == 1):
-      print('the cat is on the tree')
-      break
-
-  #i bassically do the same over and over again.  is there a more efficient way to do this?
+          print('the cat is on the tree')
+    
+      #i bassically do the same over and over again.  is there a more efficient way to do this?
     if (catlocation == 2):
-      print('the cat is on the house')
-      break
+          print('the cat is on the house')
 
     if (catlocation == 3):
       print('the cat is on the backyard')
-      break
+      
 
-var = input <= 4
-
-if var != True:
-  print('exiting program')
+print('exiting program')
 #extra code not used
 #elif (catlocation == 2):
 #print("the cat is on the house")
